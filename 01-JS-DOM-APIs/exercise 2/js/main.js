01d0a464f3ef3b2d32856c2e89cf900961521ae5
facundolastra <facundolastra94@gmail.com>
2018-10-21 20:15:18 -0300

    restructuring of folders, exercise 2

---
+++ 01-JS-DOM-APIs/exercise 2/js/main.js
@@ -0,0 +1,9 @@
+
+function fullLoad() {
+    let element = document.getElementById("hidden");
+    element.hidden = false;
+}
+
+function displayAlert(){
+    alert("You have clicked on the button, you should feel sorry.");
+}
