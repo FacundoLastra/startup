71290d5b22fe407227a25f30f147712ab9003088
FacundoLastra <facundolastra94@gmail.com>
2018-11-05 11:40:21 -0300

    RN - Topic 1 Done

--- 06-React-Native/Topic 1/App.js
+++ 06-React-Native/Topic 1/App.js
@@ -1,55 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { View } from 'react-native';
+import {ScrollViewTest} from './components/ScrollViewTest'
 
 export default class App extends React.Component {
   render() {
     return (
-     
-      <ScrollView style={scrollViewStyle.container} horizontal={true} centerContent={true}>
-        <View style={styles.container}>
-          <Text>Holis soy una APP</Text>        
-        </View>
-        <View style={styles2.container}>
-          <Text>Holis soy una APP</Text>        
-        </View>
-        <View style={styles2.container}>
-          <Text>Holis soy una APP</Text>        
-        </View>
-        <View style={styles2.container}>
-          <Text>Holis soy una APP</Text>        
-        </View>
-        <View style={styles2.container}>
-          <Text>Holis soy una APP</Text>        
-        </View>
-      </ScrollView>
-     
+      <ScrollViewTest/>     
     );
   }
 }
-const scrollViewStyle = StyleSheet.create({
-  container: {
-    paddingVertical: 20
-  }
-})
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#6ea0bf',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor:'#000000',
-    borderStyle:'solid',
-    borderWidth: 2
-  },
-});
-
-const styles2 = StyleSheet.create({
-  container: {
-    backgroundColor: '#e4e6ce',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor:'#000000',
-    borderStyle:'solid',
-    borderWidth: 2
-  },
-});
-
