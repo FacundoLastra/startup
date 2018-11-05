eae2b1b69e1c22bbd73833a849590416c80f50a7
facundolastra <facundolastra94@gmail.com>
2018-11-05 16:12:30 -0300

    exercise 5 and 6

--- 06-React-Native/Topic 2/App.js
+++ 06-React-Native/Topic 2/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { View } from 'react-native';
 //import { Exercise3 } from './components/Exercise3'
 //import { Exercise3SeconView } from './components/Exercise3SeconView'
-import { Exercise4 } from './components/Exercise4'
+//import { Exercise4 } from './components/Exercise4'
+import { Exercise5 } from './components/Exercise5'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Exercise4/>     
+      <Exercise5/>     
     );
   }
 }
---
+++ 06-React-Native/Topic 2/styles/exercise5.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Platform } from 'react-native';
+
+export const styles =  
+    StyleSheet.create({
+        container: {
+            flex: 1,
+            flexDirection: 'row',
+            backgroundColor: Platform.OS === 'ios'? "grey" : "green"
+        },
+        row1: {
+            flex:1,
+            flexDirection: "column",
+            justifyContent:"flex-start"
+        },
+        row2: {
+            flex:1,
+            flexDirection: "column",
+            justifyContent: "center"
+        },
+        row3: {
+            flex:1,
+            flexDirection: "column",
+            justifyContent:"flex-end"
+        },
+        firstSquare: {      
+            backgroundColor: "red",
+            width:110,
+            height: 110,
+            marginTop: 30,
+            marginLeft: 10
+        },
+        secondSquare: {       
+            backgroundColor: "white",
+            width:110,
+            height: 110
+        },
+        thirdSquare: {    
+            backgroundColor: "#673ab7",
+            width:110,
+            height: 110,
+            marginBottom: 10
+        },
+        squareFixes: {
+            borderRadius: 5
+        }
+    })
